61920b8415e9a569afd3612a371e9c8f3b294eae
philippzeppelin <philippzeppelin@gmail.com>
2023-04-17 22:44:56 +0300

    Added SwiftLint

--- KinoPoisk/AppDelegate.swift
+++ KinoPoisk/AppDelegate.swift
@@ -10,8 +10,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
@@ -25,4 +23,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
 }
-
