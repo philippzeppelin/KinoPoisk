d42a74df5962c3593ad9e86d4953858467d5ebfc
philippzeppelin <philippzeppelin@gmail.com>
2023-06-17 18:45:10 +0300

    Added pods

--- KinoPoisk/App/AppDelegate.swift
+++ KinoPoisk/App/AppDelegate.swift
@@ -22,6 +22,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
 
-        return true
+        return true 
     }
 }
